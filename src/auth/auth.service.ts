6c937dc717bdd5b04cb4ce6d30990f4f4e352d9e
bineetNaidu <bineet.s.naidu@gmail.com>
2021-10-05 21:14:33 +0530

    ✨ create base Auth Service

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,20 +1,35 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, Injectable, HttpStatus } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import argon2 from 'argon2';
+import { User } from 'src/user/user.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class AuthService {
+  constructor(@InjectRepository(User) private userRepo: Repository<User>) {}
+
   async login(username: string, password: string) {
-    return {
-      username,
-      password,
-    };
+    const user = await this.userRepo.findOne({ where: { username } });
+    if (!user) {
+      throw new HttpException("User Doesn't exists!", HttpStatus.BAD_REQUEST);
+    }
+
+    const validPassword = await argon2.verify(user.password, password);
+
+    if (!validPassword) {
+      throw new HttpException('UNAUTHORIZED!', HttpStatus.UNAUTHORIZED);
+    }
+
+    return user;
   }
 
   async register(username: string, password: string, is_admin: boolean) {
-    return {
-      username,
-      password,
-      is_admin,
-    };
+    const hash = await argon2.hash(password, { hashLength: 12 });
+    const user = await this.userRepo
+      .create({ username, password: hash, is_admin })
+      .save();
+
+    return user;
   }
 
   async getRefreshToken(accessToken: string) {
